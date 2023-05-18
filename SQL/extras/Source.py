7581ad4d7e8a9dc4e1383c5895f1b1dc830ad579
Source crystal <132131183+NO1BROS@users.noreply.github.com>
2023-05-18 19:53:34 +0300

    Add files via upload

---
+++ SQL/extras/Source.py
@@ -0,0 +1,11 @@
+import re
+import time
+from datetime import datetime
+from Arab import StartTime, no1bros
+from Arab.Config import Config
+from Arab.plugins import mention
+help1 = ("**🝳 ⦙ كيفيه التنصيب :**")
+help2 = ("**🝳 ⦙ قـائمـه الاوامـر :**\n**🝳 ⦙ قنـاه السـورس :** @no1bros\n**🝳 ⦙ شـرح اوامـر السـورس : @SYTHON1**\n**🝳 ⦙ شـرح فـارات السـورس : @TEAM SYTHON CRYSTAL** \n - اوامر الاونلاين تشتغل فقط في المجموعات ")
+TG_BOT = Config.TG_BOT_USERNAME
+TM = time.strftime("%I:%M")
+Sour = f"**‎⿻┊My 𖠄 {mention} ٫ **\n‌‎**⿻┊BoT 𖠄 {TG_BOT} ٫**\n**‌‎⿻┊TimE 𖠄 {TM} ٫**\n**‌‎⿻┊‌‎VeRsIoN 𖠄 (7.7) ,** \n**⿻┊‌‎𝐒𝐘𝐓𝐇𝐎𝐍  𝐂𝐑𝐘𝐒𝐓𝐀𝐋 𖠄** @no1bros"
