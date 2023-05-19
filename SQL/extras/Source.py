52513244114687e1e7731e2c49918b7d681291c8
Source crystal <132131183+NO1BROS@users.noreply.github.com>
2023-05-19 03:31:48 +0300

    Add files via upload

--- SQL/extras/Source.py
+++ SQL/extras/Source.py
@@ -1,11 +1,11 @@
 import re
 import time
 from datetime import datetime
-from Arab import StartTime, no1bros
+from Arab import StartTime, iqthon
 from Arab.Config import Config
 from Arab.plugins import mention
 help1 = ("**🝳 ⦙ كيفيه التنصيب :**")
-help2 = ("**🝳 ⦙ قـائمـه الاوامـر :**\n**🝳 ⦙ قنـاه السـورس :** @no1bros\n**🝳 ⦙ شـرح اوامـر السـورس : @SYTHON1**\n**🝳 ⦙ شـرح فـارات السـورس : @TEAM SYTHON CRYSTAL** \n - اوامر الاونلاين تشتغل فقط في المجموعات ")
+help2 = ("**🝳 ⦙ قـائمـه الاوامـر :**\n**🝳 ⦙ قنـاه السـورس :** @IQTHON\n**🝳 ⦙ شـرح اوامـر السـورس : @L3LL3**\n**🝳 ⦙ شـرح فـارات السـورس : @TEAMTELETHON** \n - اوامر الاونلاين تشتغل فقط في المجموعات ")
 TG_BOT = Config.TG_BOT_USERNAME
 TM = time.strftime("%I:%M")
-Sour = f"**‎⿻┊My 𖠄 {mention} ٫ **\n‌‎**⿻┊BoT 𖠄 {TG_BOT} ٫**\n**‌‎⿻┊TimE 𖠄 {TM} ٫**\n**‌‎⿻┊‌‎VeRsIoN 𖠄 (7.7) ,** \n**⿻┊‌‎𝐒𝐘𝐓𝐇𝐎𝐍  𝐂𝐑𝐘𝐒𝐓𝐀𝐋 𖠄** @no1bros"
+Sour = f"**‎⿻┊My 𖠄 {mention} ٫ **\n‌‎**⿻┊BoT 𖠄 {TG_BOT} ٫**\n**‌‎⿻┊TimE 𖠄 {TM} ٫**\n**‌‎⿻┊‌‎VeRsIoN 𖠄 (7.7) ,** \n**⿻┊‌‎TeLeThoN AraB 𖠄** @IQTHON"
